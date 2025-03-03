39bbda9188e32e59116b3094c8c4ba9487c4f8e9
sgandeplli <sgandepalli@softility.com>
2025-03-03 17:54:50 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -13,10 +13,4 @@ module "delegate" {
   upgrader_enabled = true
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.google_container_cluster.primary.endpoint
-    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-    token                  = data.google_container_cluster.primary.master_auth[0].access_token
-  }
-}
+
