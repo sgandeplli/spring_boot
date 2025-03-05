521db496e524190920188dbfc1159a7fe3bac984
sgandeplli <sgandepalli@softility.com>
2025-03-05 19:20:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "lyrical-bus-452711-c5"  
+  default     = "sekhar-452813"  
 }
 
 variable "region" {
